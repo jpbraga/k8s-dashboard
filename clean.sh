e6f95cbf62cbe724258a58e33cb3bc3aa8790832
Joao Braga <jp.am.braga@gmail.com>
2021-02-22 14:50:35 -0300

    initial commit

---
+++ clean.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#Install dashboard
+echo "Destroying dashboard..."
+kubectl delete -f https://raw.githubusercontent.com/kubernetes/dashboard/v2.1.0/aio/deploy/recommended.yaml
+
+#Clean up user credentials
+echo "Cleaning up old credentials..."
+kubectl -n kubernetes-dashboard delete serviceaccount admin-user
+kubectl -n kubernetes-dashboard delete clusterrolebinding admin-user
+
+#Install metrics server
+echo "Installing metrics-server..."
+kubectl delete -f k8s-dashboard.yaml
