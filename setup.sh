d1588011f573511ddecd2065cf6171fe494f89f0
Joao Braga <jp.am.braga@gmail.com>
2021-02-22 15:32:18 -0300

    Added more time for the dashboard to start

--- setup.sh
+++ setup.sh
@@ -18,7 +18,7 @@ kubectl apply -f k8s-cluster-role-binding.yaml
 echo -e "\n\nInstalling metrics-server..."
 kubectl apply -f k8s-dashboard.yaml
 
-sleep 5s
+sleep 10s
 echo -e "\n\nOpening up dashboard url..."
 open http://localhost:8001/api/v1/namespaces/kubernetes-dashboard/services/https:kubernetes-dashboard:/proxy/
 kubectl -n kubernetes-dashboard get secret $(kubectl -n kubernetes-dashboard get sa/admin-user -o jsonpath="{.secrets[0].name}") -o go-template="{{.data.token | base64decode}}" | pbcopy
