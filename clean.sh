727fd4a685262f9054c4ca4b966c88c115d7fde7
Joao Paulo Braga <jp.am.braga@gmail.com>
2021-02-23 17:10:46 -0300

    Shell scripts updated

--- clean.sh
+++ clean.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
-#Install dashboard
-echo "Destroying dashboard..."
-kubectl delete -f https://raw.githubusercontent.com/kubernetes/dashboard/v2.1.0/aio/deploy/recommended.yaml
+
+#Style vars
+bold=$(tput bold)
+normal=$(tput sgr0)
+
+#Uninstall dashboard
+echo -e "\n${bold}Destroying dashboard...${normal}"
+kubectl delete -f https://raw.githubusercontent.com/kubernetes/dashboard/v2.1.0/aio/deploy/recommended.yaml --ignore-not-found=true
 
 #Clean up user credentials
-echo "Cleaning up old credentials..."
-kubectl -n kubernetes-dashboard delete serviceaccount admin-user
-kubectl -n kubernetes-dashboard delete clusterrolebinding admin-user
+echo -e "\n${bold}Cleaning up old credentials...${normal}"
+kubectl -n kubernetes-dashboard delete serviceaccount admin-user --ignore-not-found=true
+kubectl -n kubernetes-dashboard delete clusterrolebinding admin-user --ignore-not-found=true
 
 #Install metrics server
-echo "Uninstalling metrics-server..."
-kubectl delete -f k8s-dashboard.yaml
+echo -e "\n${bold}Uninstalling metrics-server...${normal}"
+kubectl delete -f k8s-dashboard.yaml --ignore-not-found=true
--- setup.sh
+++ setup.sh
@@ -1,26 +1,62 @@
 #!/bin/bash
 
+#Style vars
+bold=$(tput bold)
+normal=$(tput sgr0)
+
 #Install dashboard
-echo "Installing dashboard..."
+echo -e "\n\n${bold}Installing dashboard...${normal}"
 kubectl apply -f https://raw.githubusercontent.com/kubernetes/dashboard/v2.1.0/aio/deploy/recommended.yaml
 
-#Clean up user credentials
-echo -e "\n\nCleaning up old credentials..."
-kubectl -n kubernetes-dashboard delete serviceaccount admin-user --ignore-not-found=true
-kubectl -n kubernetes-dashboard delete clusterrolebinding admin-user --ignore-not-found=true
-
 #Create new credentials
-echo -e "\n\nCreating new credentials..."
-kubectl apply -f k8s-dashboard-adminuser.yaml
-kubectl apply -f k8s-cluster-role-binding.yaml
+echo -e "\n\n${bold}Verifying credentials...${normal}"
+serviceaccount=$(kubectl -n kubernetes-dashboard get serviceaccount admin-user --ignore-not-found=true | grep admin-user)
+clusterrolebinding=$(kubectl -n kubernetes-dashboard get clusterrolebinding admin-user --ignore-not-found=true | grep admin-user)
+if [[ -z $serviceaccount ]]; then
+    echo -e "\n${bold}Creating a new service account admin-user...${normal}"
+    kubectl apply -f k8s-dashboard-adminuser.yaml
+fi
+
+if [[ -z $clusterrolebinding ]]; then
+    echo -e "\n${bold}Creating a new cluster role binding for the admin-user...${normal}"
+    kubectl apply -f k8s-cluster-role-binding.yaml
+fi
+
+if [[ -n $clusterrolebinding && -n $serviceaccount ]]; then
+    echo -e "Service account admin-user and cluster role binding are already in place!"
+fi
 
 #Install metrics server
-echo -e "\n\nInstalling metrics-server..."
+echo -e "\n\n${bold}Installing metrics-server...${normal}"
 kubectl apply -f k8s-dashboard.yaml
 
-sleep 10s
-echo -e "\n\nOpening up dashboard url..."
+#Waiting for the initialization
+echo -e "\n\n${bold}Waiting for the dashboard to start...${normal}"
+count=1
+while [ $count -le 10 ]; do
+
+    dpod=$(kubectl -n kubernetes-dashboard get pods -l k8s-app=kubernetes-dashboard | grep Running)
+    if [[ -n $dpod ]]; then
+        break
+    fi
+
+    fpod=$(kubectl -n kubernetes-dashboard get pods -l k8s-app=kubernetes-dashboard | grep Failed)
+    if [[ -n $fpod ]]; then
+        echo -e "${bold}There was an error while starting up the dashboard - see the log bellow${normal}\n"
+        echo $(kubectl -n kubernetes-dashboard logs -l k8s-app=kubernetes-dashboard)
+        exit 1
+    fi
+    echo "Waiting for the dashboard to start..."
+    count=$(($count + 1))
+    sleep 1s
+done
+if [[ $count -gt 10 ]]; then
+    echo "${bold}It may not have been fully initialized. The dashboard pod's current state is: ${normal}"
+    kubectl -n kubernetes-dashboard get pods -l k8s-app=kubernetes-dashboard
+fi
+
+echo -e "\n${bold}Opening up dashboard url...${normal}"
 open http://localhost:8001/api/v1/namespaces/kubernetes-dashboard/services/https:kubernetes-dashboard:/proxy/
 kubectl -n kubernetes-dashboard get secret $(kubectl -n kubernetes-dashboard get sa/admin-user -o jsonpath="{.secrets[0].name}") -o go-template="{{.data.token | base64decode}}" | pbcopy
-echo -e "\nStarting up dashboard server..."
+echo -e "\n${bold}Starting up dashboard server...${normal}"
 kubectl proxy
